cdbb3b48b32f6eca06bed6ea7ca3b15784c12726
FRancesco0004 <ffrancescoelia@gmail.com>
2026-05-10 20:34:12 +0200

    Piccola correzione

--- src/main/java/com/example/sitoartepsaw/repository/OggettoRepository.java
+++ src/main/java/com/example/sitoartepsaw/repository/OggettoRepository.java
@@ -3,7 +3,9 @@ package com.example.sitoartepsaw.repository;
 import com.example.sitoartepsaw.entity.Oggetto;
 import com.example.sitoartepsaw.enums.StatoOggetto;
 import com.example.sitoartepsaw.enums.TipoOpera;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +20,7 @@ public interface OggettoRepository extends JpaRepository<Oggetto, Integer> {
     List<Oggetto> findByStatoAndTipoOpera(StatoOggetto stato, TipoOpera tipoOpera);
 
     // Unica eccezione con @Query — la randomica serve alla Facade
+    @Lock(LockModeType.PESSIMISTIC_WRITE) // Ai fini dell'esame va bene ma con tanti utenti crea colli di bottiglia
     @Query("SELECT o FROM Oggetto o WHERE o.stato = 'DISPONIBILE' ORDER BY RAND()")
     List<Oggetto> findAllDisponibiliRandom();
 }
